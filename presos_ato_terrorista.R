a0a3cc3c02ffaaa3478ba2d2132b7b40328f90cc
SoaresAlisson <soares.alisson@yahooo.com.br>
2023-01-19 01:17:16 -0300

    presos por dia

--- presos_ato_terrorista.R
+++ presos_ato_terrorista.R
@@ -3,42 +3,92 @@ library(stringr)
 library(rvest)
 
 url = "https://seape.df.gov.br/prisoes-dos-atentados-bsb/"
+
 pagina <- rvest::read_html(url) 
-pagina |> rvest::html_element("em") |> rvest::html_text() # ultima atualizacao
+
+#ultimaAtualizacao <- pagina |> rvest::html_element("em") |> rvest::html_text() # ultima atualizacao
+#ultimaAtualizacao
+#mesDia <- str_extract(ultimaAtualizacao, "[\\d\\/]+") |> str_replace("(.*)/(.*)", "\\2\\1")
+
+ultimaAtualizacao <- pagina |> rvest::html_element(".margin-top-60") |> rvest::html_text() |> str_remove(".*Atualizado") |>  str_extract("\\d{2}/\\d{2}/\\d+")# ultima atualizacao
+mesDia <- ultimaAtualizacao |> str_replace("(.*)/(.*)/(.*)", "\\2\\1")
 urlPDF <- pagina |> rvest::html_element("#conteudo a") |> rvest::html_attr("href")
+arqsPDF <- list.files("arquivos", pattern = "^\\d{4}_.*pdf$")
 
-tabela0 <- pdftools::pdf_text(urlPDF)
+# salvar novo pdf
+nomeNovoArquivoPDF <- paste0(mesDia, "_",
+                             str_replace(urlPDF, ".*/(.*pdf)", "\\1"))
+# arquivo já existe? se não, baixe
+ifelse((nomeNovoArquivoPDF %in% arqsPDF), 
+       str_glue('Arquivo "{nomeNovoArquivoPDF}" já existente!'),
+       download.file(urlPDF, str_glue("arquivos/{nomeNovoArquivoPDF}"))
+)
 
-tabela <- tabela0 |> unlist() |> 
+parser_tabela <- function(arqPDF){
+  pdftools::pdf_text(str_glue("arquivos/{arqPDF}")) |>
+  unlist() |> 
   strsplit("\n") |> unlist() |> 
-  str_remove_all("^ +| +$") |> 
+  str_remove_all("^ +") |> str_remove_all(" +$") |> 
   str_subset("\\d") |>
   as.data.frame() |> rename(id =1) |>
   tidyr::extract(id, regex = "(^\\d+) (.*)", into = c("id", "nome0" )) |> 
-  mutate(data = str_extract(nome0, "[\\d\\.]+"),
-         nome2 = str_remove(nome0, "[\\d\\.]+") |> str_remove(" +$"),
-         ano = stringr::str_extract(data, "\\d{4}$")) |>
-  filter() |>
+  mutate(nascimento = str_extract(nome0, "[\\d\\.]+") |> as.Date(format = "%d.%m.%Y"),
+         nome2 = str_remove(nome0, "[\\d\\.]+") |> str_remove("^ +") |> str_remove(" +$"),
+         ano = lubridate::year(nascimento)
+         ) |>
   filter(nome2 != "") |>
-  select(id, nome2, data, ano) |> rename(nome = nome2) 
+  select(nome2, nascimento, ano) |> rename(nome = nome2) 
+}
 
-#--  busca por query
-termo = "cesa"
-tabela$nome[grepl(termo, tabela$nome, ignore.case = T)]
+# exemplo de tabela
+parser_tabela(arqsPDF[3]) |> str()
+parser_tabela(arqsPDF[1]) 
 
-# Pensionistas
-pensionistas <- read.csv("~/Documentos/Programação/data/politica/202001_Pensionistas_DEFESA/202001_Cadastro.csv", 
-                         header = T, sep = ";", stringsAsFactors = F, fileEncoding="latin1") %>% as_tibble()
+# ------
+pdf = arqsPDF[1] # temporario
+nomeCol <- str_extract(pdf, "^\\d{4}") |> str_replace("^(\\d{2})(\\d{2})", "\\2.\\1") 
+tabela <- parser_tabela(pdf) |> mutate({{nomeCol}} := "sim") 
 
-nomesTabela <- stringr::str_to_title(tabela$nome) 
-nomesPensio <-  stringr::str_to_title(pensionistas$NOME)
+for (i in 2:length(arqsPDF)){
+  pdf = arqsPDF[i] # temporario
+  nomeCol <- str_extract(pdf, "^\\d{4}") |> str_replace("^(\\d{2})(\\d{2})", "\\2.\\1") 
+  tabela2 <- parser_tabela(pdf) |>mutate({{nomeCol}} := "sim") |> select(nome,nascimento, {nomeCol})
+  tabela <- full_join(tabela, tabela2, by="nome")
+}
+tabela <- arrange(tabela, nome)
+#----
 
-nomesTabela[nomesTabela %in%  nomesPensio]
 
+# ---
+nomeNascimento <- parser_tabela(arqsPDF[1]) |> select(nome,nascimento)
+for (i in 2:length(arqsPDF)){
+  nomeNascimento2 <- parser_tabela(arqsPDF[i]) |> select(nome,nascimento)
+  nomeNascimento <- union(nomeNascimento, nomeNascimento2)
+}
+nomeNascimento <- arrange(nomeNascimento, nome)
+
+tabela <- nomeNascimento
+for (i in 1:length(arqsPDF)){
+  nomeCol <- str_extract(arqsPDF[i], "^\\d{4}") |> str_replace("^(\\d{2})(\\d{2})", "\\2.\\1") 
+  tabela2 <- parser_tabela(arqsPDF[i]) |> mutate({{nomeCol}} := "sim") |> select(nome, {nomeCol})
+  tabela <- left_join(tabela, tabela2, by="nome")
+}
+str(tabela)
+# ----
+
+saveRDS(tabela, "presos_atos_golpistas.rds")
 write.csv(tabela, "presos_atos_golpistas.csv")
 
+
+#--  busca por query
+termo = "maria"
+tabela$nome[grepl(termo, tabela$nome, ignore.case = T)]
+# ----
+
+
+# cruzar dados com financiadores
 financiadores <- readLines('arquivos/financiadores.txt')  |> strsplit("\\n") |> unlist() |>
   str_remove("^ +") |> str_remove("^#.*") |> str_remove(",.*")
 
-
 nomesTabela[nomesTabela %in% financiadores]
+# ----
